bb45f087efd89ce21479a7d73be7d95d6da977fb
Utkarsh Tiwari <utkarshweb2023@gmail.com>
2025-09-20 14:27:03 +0530

    Add bencode and dgram dependencies; Implement torrent file reading and UDP messaging

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log("Hello via Bun!");
+import fs from "fs";
+import bencode from "bencode";
+import dgram from "dgram";
+import { Buffer } from "buffer";
+
+
+const torrent_buffer = fs.readFileSync("puppy.torrent");
+const torrent = bencode.decode(torrent_buffer);
+
+const announceUrl = Buffer.from(torrent.announce).toString("utf-8");
+const url = new URL(announceUrl);
+
+const socket = dgram.createSocket("udp4");
+const myMsg = Buffer.from("Hello, World!", "utf-8");
+
+socket.send(myMsg, 0, myMsg.length, Number.parseInt(url.port), url.hostname, ()=>{})
+
+socket.on("message", msg => {
+    console.log(msg.toString("utf-8"));
+})
